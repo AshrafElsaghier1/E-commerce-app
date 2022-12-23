08d75cf59844171a4d74f60112930114e855e229
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2022-12-24 00:22:08 +0200

    Updates

--- src/components/helmet/Helmet.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Helmet = ({ children, title }) => {
-  document.title = title ? `E-commerce-app | ${title}` : "";
-  return <>{children}</>;
-};
-
-export default Helmet;
